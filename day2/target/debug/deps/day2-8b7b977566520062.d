f59b45884e9e1d805d24c7d4101273fe18605aed
Yurevdb <vanderbruggen.yure@gmail.com>
2023-06-04 13:39:39 +0200

    Day 2

---
+++ day2/target/debug/deps/day2-8b7b977566520062.d
@@ -0,0 +1,5 @@
+/home/vy/dev/aoc2022-rust/day2/target/debug/deps/day2-8b7b977566520062.rmeta: src/main.rs
+
+/home/vy/dev/aoc2022-rust/day2/target/debug/deps/day2-8b7b977566520062.d: src/main.rs
+
+src/main.rs:
---
+++ day2/target/debug/deps/day2-ce466e84c61d2d4e.d
@@ -0,0 +1,5 @@
+/home/vy/dev/aoc2022-rust/day2/target/debug/deps/day2-ce466e84c61d2d4e: src/main.rs
+
+/home/vy/dev/aoc2022-rust/day2/target/debug/deps/day2-ce466e84c61d2d4e.d: src/main.rs
+
+src/main.rs:
