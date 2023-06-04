d56f88415b74bcb870fcabe849b25fb3b89ac50d
Yurevdb <vanderbruggen.yure@gmail.com>
2023-06-04 13:45:09 +0200

    idk anymore

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/home/vy/dev/aoc2022-rust/day2/target/debug/day2: /home/vy/dev/aoc2022-rust/day2/src/main.rs
--- day2/target/debug/deps/day2-ce466e84c61d2d4e.d
+++
@@ -1,5 +0,0 @@
-/home/vy/dev/aoc2022-rust/day2/target/debug/deps/day2-ce466e84c61d2d4e: src/main.rs
-
-/home/vy/dev/aoc2022-rust/day2/target/debug/deps/day2-ce466e84c61d2d4e.d: src/main.rs
-
-src/main.rs:
